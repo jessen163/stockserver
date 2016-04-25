8fe33ce4cdd5f158c933aa6765805fdf64502506
rydsongby <linghaio_2011@163.com>
2016-04-25 17:22:20 +0800

    dao

--- core/stock-system-service-impl/src/main/java/com/ryd/system/service/impl/StDateScheduleServiceImpl.java
+++ core/stock-system-service-impl/src/main/java/com/ryd/system/service/impl/StDateScheduleServiceImpl.java
@@ -89,6 +89,19 @@ public class StDateScheduleServiceImpl implements StDateScheduleService {
 
     @Override
     public boolean getIsCanQuote() {
+        //如果当前时间是节假日
+        if (getIsFestival()) {
+            return false;
+        }
+        if(getDateAndTimeJudge() == Constant.STQUOTE_TRADE_TIMECOMPARE_1.intValue() ||
+                getDateAndTimeJudge() == Constant.STQUOTE_TRADE_TIMECOMPARE_2.intValue()){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean getIsTimeCanQuote() {
         if(getDateAndTimeJudge() == Constant.STQUOTE_TRADE_TIMECOMPARE_1.intValue() ||
                 getDateAndTimeJudge() == Constant.STQUOTE_TRADE_TIMECOMPARE_2.intValue()){
             return true;
@@ -112,29 +125,26 @@ public class StDateScheduleServiceImpl implements StDateScheduleService {
         String restTimeStart = stSystemParamService.getParamByKey(CacheConstant.CACHEKEY_SYSTEM_CONFIG_REST_TIME_START);
         String restTimeEnd = stSystemParamService.getParamByKey(CacheConstant.CACHEKEY_SYSTEM_CONFIG_REST_TIME_END);
         String closeTime = stSystemParamService.getParamByKey(CacheConstant.CACHEKEY_SYSTEM_CONFIG_CLOSE_TIME);
-        //如果当前时间是工作日
-        if (!getIsFestival()) {
 
-            long tnow = calNow.getTime().getTime();
-            //开盘时间
-            long t9 = DateUtils.getSetHourTime(openTime).getTime();
-            //休盘时间开始
-            long t11 = DateUtils.getSetHourTime(restTimeStart).getTime();
-            //休盘时间结束
-            long t13 = DateUtils.getSetHourTime(restTimeEnd).getTime();
-            //收盘时间
-            long t15 = DateUtils.getSetHourTime(closeTime).getTime();
+        long tnow = calNow.getTime().getTime();
+        //开盘时间
+        long t9 = DateUtils.getSetHourTime(openTime).getTime();
+        //休盘时间开始
+        long t11 = DateUtils.getSetHourTime(restTimeStart).getTime();
+        //休盘时间结束
+        long t13 = DateUtils.getSetHourTime(restTimeEnd).getTime();
+        //收盘时间
+        long t15 = DateUtils.getSetHourTime(closeTime).getTime();
 
-            //如果当前时间在上午9：30~11：30之间，或者下午1：00~3：00之间，可以交易和报价
-            if ((t9 < tnow && tnow < t11) || (t13 < tnow && tnow < t15)) {
-                return Constant.STQUOTE_TRADE_TIMECOMPARE_1;
-            } else if ((t11 < tnow && tnow < t13)) {//如果当前时间在11：30~13：00之间只允许报价
-                return Constant.STQUOTE_TRADE_TIMECOMPARE_2;
-            } else {//之外的时间，不允许报价，不允许交易
-                return Constant.STQUOTE_TRADE_TIMECOMPARE_3;
-            }
+        //如果当前时间在上午9：30~11：30之间，或者下午1：00~3：00之间，可以交易和报价
+        if ((t9 < tnow && tnow < t11) || (t13 < tnow && tnow < t15)) {
+            return Constant.STQUOTE_TRADE_TIMECOMPARE_1;
+        } else if ((t11 < tnow && tnow < t13)) {//如果当前时间在11：30~13：00之间只允许报价
+            return Constant.STQUOTE_TRADE_TIMECOMPARE_2;
+        } else {//之外的时间，不允许报价，不允许交易
+            return Constant.STQUOTE_TRADE_TIMECOMPARE_3;
         }
-        return Constant.STQUOTE_TRADE_TIMECOMPARE_3;
+
     }
 
 }
