644576796805fb5e30510add43f15b20a2090483
jessen <jessen163@gmail.com>
2016-04-25 16:02:52 +0800

    初始化

---
+++ core/stock-business-api/src/main/java/com/ryd/business/service/StStockConfigService.java
@@ -0,0 +1,30 @@
+package com.ryd.business.service;
+
+import com.ryd.business.model.StStockConfig;
+
+import java.util.List;
+
+/**
+ * <p>标题:股票配置Service</p>
+ * <p>描述:</p>
+ * 包名：com.ryd.business.service
+ * 创建人：chenji
+ * 创建时间：2016/4/24 15:11
+ */
+public interface StStockConfigService {
+    /**
+     * 股票基础信息
+     * @param stStockConfig
+     * @param pageIndex
+     * @param limit
+     * @return
+     */
+    public List<StStockConfig> findStockConfig(StStockConfig stStockConfig, int pageIndex, int limit);
+
+    /**
+     * 通过id查询股票配置信息
+     * @param stStockConfig
+     * @return
+     */
+    public StStockConfig findStockConfigById(StStockConfig stStockConfig);
+}
---
+++ core/stock-business-service-impl/src/main/java/com/ryd/business/service/impl/StStockConfigServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ryd.business.service.impl;
+
+import com.ryd.business.dao.StStockConfigDao;
+import com.ryd.business.model.StStockConfig;
+import com.ryd.business.service.StStockConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * <p>标题:股票配置业务实现类</p>
+ * <p>描述:股票配置业务实现类</p>
+ * 包名：com.ryd.business.service.impl
+ * 创建人：chenji
+ * 创建时间：2016/4/25 10:11
+ */
+public class StStockConfigServiceImpl implements StStockConfigService {
+    @Autowired
+    private StStockConfigDao stStockConfigDao;
+
+    @Override
+    public List<StStockConfig> findStockConfig(StStockConfig stStockConfig, int pageIndex, int limit) {
+        int offset = (pageIndex-1)*limit;
+        List<StStockConfig> stStockConfigList = stStockConfigDao.getTList(stStockConfig, limit, offset);
+        return stStockConfigList;
+    }
+
+    @Override
+    public StStockConfig findStockConfigById(StStockConfig stStockConfig) {
+        return stStockConfigDao.getTById(stStockConfig);
+    }
+}
