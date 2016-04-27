7832ee4a004d0a3ab5e842bdeb7f27f7033e95ba
rydsongby <linghaio_2011@163.com>
2016-04-27 18:26:49 +0800

    dao

---
+++ core/stock-business-service-impl/src/main/java/com/ryd/business/service/impl/StMoneyJournalServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ryd.business.service.impl;
+
+import com.ryd.business.dao.StMoneyJournalDao;
+import com.ryd.business.dto.SearchMoneyJournalDTO;
+import com.ryd.business.model.StMoneyJournal;
+import com.ryd.business.service.StMoneyJournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * <p>标题:</p>
+ * <p>描述:</p>
+ * 包名：com.ryd.business.service.impl
+ * 创建人：songby
+ * 创建时间：2016/4/27 18:00
+ */
+public class StMoneyJournalServiceImpl implements StMoneyJournalService {
+
+    @Autowired
+    private StMoneyJournalDao stMoneyJournalDao;
+
+    @Override
+    public boolean saveMoneyJournalList(List<StMoneyJournal> moneyJournalList) {
+        return stMoneyJournalDao.addBatch(moneyJournalList) > 0;
+    }
+
+    @Override
+    public List<StMoneyJournal> findMoneyJournalList(SearchMoneyJournalDTO searchMoneyJournalDTO) {
+        return null;
+    }
+}
