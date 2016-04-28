dd5f38f92e1126c927c76cf984264af86c3ccbc6
rydsongby <linghaio_2011@163.com>
2016-04-28 14:36:54 +0800

    dao

--- core/base-common/src/main/java/com/ryd/basecommon/util/CacheConstant.java
+++ core/base-common/src/main/java/com/ryd/basecommon/util/CacheConstant.java
@@ -21,21 +21,21 @@ public class CacheConstant {
     public static final String CACHEKEY_SYSTEM_CONFIG_REST_TIME_START  = "stockMarketRestTimeStart";
 
     // 缓存key-系统配置参数信息-股市休盘时间结束
-    public static final String CACHEKEY_SYSTEM_CONFIG_REST_TIME_END  = "stockMarketRestTimeStart";
+    public static final String CACHEKEY_SYSTEM_CONFIG_REST_TIME_END  = "stockMarketRestTimeEnd";
 
     // 缓存key-系统配置参数信息-股市收盘时间
     public static final String CACHEKEY_SYSTEM_CONFIG_CLOSE_TIME  = "stockMarketCloseTime";
 
-    // 缓存key-系统配置参数信息-股市结算时间
+    // 缓存key-系统配置参数信息-佣金比例
     public static final String CACHEKEY_SYSTEM_COMMINSSION_PERCENT  = "stockCommissionPercent";
 
-    // 缓存key-系统配置参数信息-佣金比例
+    // 缓存key-系统配置参数信息-印花税比例
     public static final String CACHEKEY_SYSTEM_CONFIG_TAX_PERCENT  = "stockTaxPercent";
 
-    // 缓存key-系统配置参数信息-印花税比例
+    // 缓存key-系统配置参数信息-股票涨跌幅度
     public static final String CACHEKEY_SYSTEM_CONFIG_UPANDDOWN_PERCENT  = "stockUpAndDownPercent";
 
-    // 缓存key-系统配置参数信息-股票涨跌幅度
+    // 缓存key-系统配置参数信息-股市结算时间
     public static final String CACHEKEY_SYSTEM_CONFIG_SETTLE_TIME  = "stockMarketSettleTime";
 
     // 缓存key-节假日
