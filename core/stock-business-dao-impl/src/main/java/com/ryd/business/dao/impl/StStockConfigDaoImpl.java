3e2d277f7c373e2fa42bd3c814190df3cd1c5a07
rydsongby <linghaio_2011@163.com>
2016-04-28 14:38:17 +0800

    dao

--- core/stock-business-dao-impl/src/main/java/com/ryd/business/dao/impl/StStockConfigDaoImpl.java
+++ core/stock-business-dao-impl/src/main/java/com/ryd/business/dao/impl/StStockConfigDaoImpl.java
@@ -3,6 +3,7 @@ package com.ryd.business.dao.impl;
 import com.ryd.business.dao.StStockConfigDao;
 import com.ryd.business.model.StStockConfig;
 import com.ryd.business.mybatis.StStockConfigMapper;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -59,5 +60,13 @@ public class StStockConfigDaoImpl implements StStockConfigDao {
             return -1;
         }
         return stStockConfigMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int addBatch(List<StStockConfig> list) {
+        if(CollectionUtils.isNotEmpty(list)){
+            return stStockConfigMapper.insertBatch(list);
+        }
+        return -1;
     }
 }
