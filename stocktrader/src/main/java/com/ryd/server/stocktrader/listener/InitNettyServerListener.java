35b7863285a5ae6b3ae069ad01b6236200090292
jessen <jessen163@gmail.com>
2016-04-28 16:33:09 +0800

    配置端口

--- stocktrader/src/main/java/com/ryd/server/stocktrader/listener/InitNettyServerListener.java
+++ stocktrader/src/main/java/com/ryd/server/stocktrader/listener/InitNettyServerListener.java
@@ -2,6 +2,7 @@ package com.ryd.server.stocktrader.listener;
 
 import com.ryd.server.stocktrader.net.StockTraderServer;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -12,8 +13,10 @@ import javax.servlet.ServletContextListener;
 public class InitNettyServerListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext sc = servletContextEvent.getServletContext();//由事件得到servletcontext
+        Integer stockserverPort = Integer.parseInt(sc.getInitParameter("stockserver.port"));
         try {
-            new StockTraderServer().bind(9999);
+            new StockTraderServer().bind(stockserverPort);
         } catch (Exception e) {
             e.printStackTrace();
         }
