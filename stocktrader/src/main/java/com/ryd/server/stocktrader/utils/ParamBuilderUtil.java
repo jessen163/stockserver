302bbd35def4e328c4f66fa2ef674edcd26b757d
rydsongby <linghaio_2011@163.com>
2016-04-29 10:08:11 +0800

    dao

--- stocktrader/src/main/java/com/ryd/server/stocktrader/utils/ParamBuilderUtil.java
+++ stocktrader/src/main/java/com/ryd/server/stocktrader/utils/ParamBuilderUtil.java
@@ -6,7 +6,7 @@ import com.ryd.basecommon.util.DateUtils;
 import com.ryd.business.model.*;
 
 /**
- * <p>标题:</p>
+ * <p>标题:参数封闭工具类</p>
  * <p>描述:</p>
  * 包名：com.ryd.server.stocktrader.utils
  * 创建人：songby
@@ -14,6 +14,11 @@ import com.ryd.business.model.*;
  */
 public class ParamBuilderUtil {
 
+    /**
+     * 股票信息
+     * @param stStock
+     * @return
+     */
     public static DiyNettyMessage.StockInfo.Builder getStockInfoBuilder(StStock stStock){
 
         DiyNettyMessage.StockInfo.Builder builder = DiyNettyMessage.StockInfo.newBuilder();
@@ -57,6 +62,11 @@ public class ParamBuilderUtil {
         return builder;
     }
 
+    /**
+     * 股票价格信息
+     * @param stStock
+     * @return
+     */
     public static DiyNettyMessage.StockPriceInfo.Builder getStockPriceInfoBuilder(StStock stStock){
         DiyNettyMessage.StockPriceInfo.Builder builder = DiyNettyMessage.StockPriceInfo.newBuilder();
         builder.setId(stStock.getId());
@@ -65,6 +75,11 @@ public class ParamBuilderUtil {
         return builder;
     }
 
+    /**
+     * 帐户信息
+     * @param stAcc
+     * @return
+     */
     public static DiyNettyMessage.AccountInfo.Builder getAccountInfoBuilder(StAccount stAcc){
 
         DiyNettyMessage.AccountInfo.Builder accbuilder = DiyNettyMessage.AccountInfo.newBuilder();
@@ -83,6 +98,11 @@ public class ParamBuilderUtil {
         return accbuilder;
     }
 
+    /**
+     * 仓位信息
+     * @param sp
+     * @return
+     */
     public static DiyNettyMessage.PositionInfo.Builder getPositionInfoBuilder(StPosition sp){
         DiyNettyMessage.PositionInfo.Builder pbuilder = DiyNettyMessage.PositionInfo.newBuilder();
         pbuilder.setStockId(sp.getStockId());
@@ -98,6 +118,11 @@ public class ParamBuilderUtil {
         return pbuilder;
     }
 
+    /**
+     * 报价信息
+     * @param q1
+     * @return
+     */
     public static DiyNettyMessage.QuoteInfo.Builder getQuoteInfoBuilder(StQuote q1){
 
         DiyNettyMessage.QuoteInfo.Builder qbuiler =  DiyNettyMessage.QuoteInfo.newBuilder();
@@ -117,6 +142,12 @@ public class ParamBuilderUtil {
         return qbuiler;
     }
 
+    /**
+     * 成交记录信息
+     * @param r1
+     * @param accountId
+     * @return
+     */
     public static DiyNettyMessage.TradeRecordInfo.Builder getTradeRecordInfoBuilder(StTradeRecord r1, String accountId){
 
         DiyNettyMessage.TradeRecordInfo.Builder tbuiler = DiyNettyMessage.TradeRecordInfo.newBuilder();
@@ -139,6 +170,11 @@ public class ParamBuilderUtil {
         return tbuiler;
     }
 
+    /**
+     * 资金流水信息
+     * @param journal
+     * @return
+     */
     public static DiyNettyMessage.MoneyJournalInfo.Builder getMoneyJournalInfoBuilder(StMoneyJournal journal){
 
         DiyNettyMessage.MoneyJournalInfo.Builder mbuiler = DiyNettyMessage.MoneyJournalInfo.newBuilder();
