e8051fd0dc9890c7cac510d0aa50d0f6d484628d
rydsongby <linghaio_2011@163.com>
2016-05-04 15:47:23 +0800

    dao

--- automatedtrade/src/main/java/com/ryd/server/automatedtrade/controller/TradeController.java
+++ automatedtrade/src/main/java/com/ryd/server/automatedtrade/controller/TradeController.java
@@ -1,43 +1,95 @@
 package com.ryd.server.automatedtrade.controller;
 
+import com.ryd.basecommon.util.ApplicationConstants;
+import com.ryd.basecommon.util.CacheConstant;
 import com.ryd.business.dto.AutomatedTradingDTO;
 import com.ryd.business.service.AutomatedTradingService;
+import com.ryd.server.automatedtrade.thread.AutoTradeThread;
+import com.ryd.system.service.StSystemParamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 自动化交易
  * Created by Administrator on 2016/5/3.
  */
 @Controller
+@RequestMapping("/autotrade")
 public class TradeController {
     @Autowired
     private AutomatedTradingService automatedTradingService;
+    @Autowired
+    private StSystemParamService stSystemParamService;
+
+    private AutoTradeThread thread = null;
+    private ScheduledExecutorService autoService = null;
 
     public TradeController() {
-        System.out.println("TradeController");
+        thread = new AutoTradeThread(automatedTradingService);
     }
 
     /**
      * 交易主界面
      * @return
      */
-    @RequestMapping("/index1")
-    public String tradeIndex() {
-        return "index";
+    @RequestMapping("/index")
+    public ModelAndView tradeIndex() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+
+        return mv;
     }
 
     /**
      * 提交报价
-     * @param accountId
-     * @param stockId
+     * @param accountNum
+     * @param stockCode
      * @return
      */
-    public String saveTrading(String accountId, String stockId) {
-        AutomatedTradingDTO automatedTradingDTO = new AutomatedTradingDTO();
-        automatedTradingDTO.setAccountId(accountId);
-        automatedTradingService.addAutomatedTradingByStock(automatedTradingDTO);
-        return null;
+    @RequestMapping(value = "/ajax/saveTrading", method = RequestMethod.GET)
+    @ResponseBody
+    public String saveTrading(
+            @RequestParam("accountNum") String accountNum,
+            @RequestParam("stockCode") String stockCode
+    ) {
+        long interval = Long.parseLong(stSystemParamService.getParamByKey(CacheConstant.CACHEKEY_SYSTEM_CONFIG_AUTOTRADE_INTERVAL));
+        if(ApplicationConstants.isAutoTradeMainThreadStop) {
+            ApplicationConstants.isAutoTradeMainThreadStop = false;
+            AutomatedTradingDTO automatedTradingDTO = new AutomatedTradingDTO();
+            automatedTradingDTO.setAccountId(accountNum);
+            automatedTradingDTO.setStockId(stockCode);
+
+            thread.setData(automatedTradingDTO);
+            if(autoService == null || autoService.isShutdown()) {
+                autoService = Executors.newSingleThreadScheduledExecutor();
+                autoService.scheduleAtFixedRate(thread, 0, interval, TimeUnit.SECONDS);
+            }
+            return "开始成功";
+        }else{
+            return "自动化交易已经开始";
+        }
+    }
+
+    /**
+     * 结束报价
+     * @return
+     */
+    @RequestMapping(value = "/ajax/stopTrading", method = RequestMethod.GET)
+    @ResponseBody
+    public String stopTrading() {
+        ApplicationConstants.isAutoTradeMainThreadStop = true;
+        autoService.shutdownNow();
+        return "结束成功";
     }
 }
--- automatedtrade/src/main/webapp/WEB-INF/jsp/index.jsp
+++ automatedtrade/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -8,9 +8,67 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+     <title></title>
+     <script type="text/javascript" src="/autotrade/js/jquery-1.11.1.min.js"></script>
+     <script type="text/javascript">
+       function start(){
+         var url = 'ajax/saveTrading.htm';
+         var accountNum = $("#accountNum").val();
+         var stockCode = $("#stockCode").val();
+         var dataText = {
+           "accountNum":accountNum,
+           "stockCode":stockCode
+         };
+
+         $.ajax({
+           datatype: "json",
+           type: 'get',
+           url: url,
+           data: dataText,
+           success: function (data) {
+             alert(data);
+           }
+         })
+       }
+
+       function stop(){
+         var url = 'ajax/stopTrading.htm';
+         $.ajax({
+           datatype: "json",
+           type: 'get',
+           url: url,
+           data: "",
+           success: function (data) {
+             alert(data);
+           }
+         })
+       }
+     </script>
 </head>
 <body>
-自动化交易主界面
+    <div>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="34" height="40"></td>
+            <td height="15" colspan="5">自动化交易主界面</td>
+          </tr>
+          <tr>
+            <td width="34" height="40"></td>
+            <td width="80">帐号</td>
+            <td width="180">
+              <input type="text" id="accountNum" name="accountNum" value="">
+            </td>
+            <td width="80">股票代码</td>
+            <td width="180">
+              <input type="text" id="stockCode" name="stockCode" value="">
+            </td>
+            <td>
+              <button type="button" id="startBtn" onclick="start()">开始</button>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <button type="button" id="endBtn" onclick="stop()">结束</button>
+            </td>
+          </tr>
+        </table>
+      </div>
 </body>
 </html>
