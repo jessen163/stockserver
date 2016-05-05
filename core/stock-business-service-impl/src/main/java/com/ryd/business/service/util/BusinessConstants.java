00defaa6d0bbf01de5d7e1432a68d87172f98b32
jessen <jessen163@gmail.com>
2016-05-05 12:09:30 +0800

    交易

--- core/stock-business-service-impl/src/main/java/com/ryd/business/service/util/BusinessConstants.java
+++ core/stock-business-service-impl/src/main/java/com/ryd/business/service/util/BusinessConstants.java
@@ -3,6 +3,7 @@ package com.ryd.business.service.util;
 import com.ryd.business.dto.SimulationQuoteDTO;
 import com.ryd.business.dto.StTradeQueueDTO;
 import com.ryd.business.model.StStock;
+import com.ryd.business.model.StStockConfig;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,9 +13,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by chenji on 2016/5/3.
  */
 public class BusinessConstants {
+    // 股票配置信息
+    public static ConcurrentHashMap<String, StStockConfig> stockConfigMap = new ConcurrentHashMap<String, StStockConfig>();
     public static ConcurrentHashMap<String, List<SimulationQuoteDTO>> simulateQuoteMap = new ConcurrentHashMap();
-    public static ConcurrentHashMap<String, List<StStock>> stockPriceMap = new ConcurrentHashMap<String, List<StStock>>();
-    public static ConcurrentHashMap<String, List<StStock>> tempStockPriceMap = new ConcurrentHashMap<String, List<StStock>>();
+    public static ConcurrentHashMap<String, StStock> stockPriceMap = new ConcurrentHashMap<String, StStock>();
+    public static ConcurrentHashMap<String, StStock> tempStockPriceMap = new ConcurrentHashMap<String, StStock>();
 
     //队列-多只股票
     public static ConcurrentHashMap<String,StTradeQueueDTO> stTradeQueueMap = new ConcurrentHashMap<String,StTradeQueueDTO>();
