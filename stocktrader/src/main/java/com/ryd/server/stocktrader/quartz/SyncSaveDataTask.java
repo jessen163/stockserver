8ba2254ea72a7bf00bfd66862a6f9b7c34a7d252
jessen <jessen163@gmail.com>
2016-05-06 11:17:22 +0800

    交易

---
+++ stocktrader/src/main/java/com/ryd/server/stocktrader/quartz/SyncSaveDataTask.java
@@ -0,0 +1,12 @@
+package com.ryd.server.stocktrader.quartz;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 异步保存数据
+ * Created by chenji on 2016/5/6.
+ */
+@Component
+public class SyncSaveDataTask {
+
+}
