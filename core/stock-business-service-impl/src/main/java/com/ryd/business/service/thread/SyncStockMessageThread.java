500af63a4a85bee7cc7fb2976be50e9a76e59dca
jessen <jessen163@gmail.com>
2016-05-06 11:22:38 +0800

    交易

---
+++ core/stock-business-service-impl/src/main/java/com/ryd/business/service/thread/SyncStockMessageThread.java
@@ -0,0 +1,27 @@
+package com.ryd.business.service.thread;
+
+import com.ryd.basecommon.util.ApplicationConstants;
+import com.ryd.basecommon.util.FileUtils;
+import com.ryd.business.model.StStock;
+import com.ryd.messagequeue.service.IMessageQueue;
+
+import java.util.List;
+
+/**
+ * 发送股票信息到Kafka
+ * Created by chenji on 2016/5/6.
+ */
+public class SyncStockMessageThread implements Runnable {
+    private IMessageQueue iMessageQueue;
+    private List<StStock> stockList;
+
+    public SyncStockMessageThread(IMessageQueue iMessageQueue, List<StStock> stockList) {
+        this.iMessageQueue = iMessageQueue;
+        this.stockList = stockList;
+    }
+
+    @Override
+    public void run() {
+        iMessageQueue.sendMessage(ApplicationConstants.PUSHMESSAGE_STOCKPRICELIST, FileUtils.objectToByte(stockList));
+    }
+}
