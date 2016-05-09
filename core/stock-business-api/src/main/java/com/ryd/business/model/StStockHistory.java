65db3a1437ee24e634f4a64a825cd78100fb718a
jessen <jessen163@gmail.com>
2016-05-09 11:42:15 +0800

    交易

--- core/stock-business-api/src/main/java/com/ryd/business/model/StStockHistory.java
+++ core/stock-business-api/src/main/java/com/ryd/business/model/StStockHistory.java
@@ -1,12 +1,27 @@
 package com.ryd.business.model;
 
+import com.bugull.mongo.BuguEntity;
+import com.bugull.mongo.annotations.Entity;
+import com.bugull.mongo.annotations.Id;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class StStockHistory {
+
+/**
+ * <p>标题:股票信息-历史数据表</p>
+ * <p>描述:股票信息-历史数据表</p>
+ * 包名：com.ryd.basecommon.common
+ * 创建人：songby
+ * 创建时间：2016/4/21 10:29
+ */
+@Entity
+public class StStockHistory implements Serializable, BuguEntity{
 
     private static final long serialVersionUID = -5808387262109584648L;
 
+    @Id
     private String stockId;
     //股票名称
     private String stockName;
@@ -345,5 +360,15 @@ public class StStockHistory {
 
     public void setStockTime(Date stockTime) {
         this.stockTime = stockTime;
+    }
+
+    @Override
+    public void setId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    @Override
+    public String getId() {
+        return this.stockId;
     }
 }
